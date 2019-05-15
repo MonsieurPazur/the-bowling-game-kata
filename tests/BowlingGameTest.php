1dd93d4cc87b30c973b15e2b15e96a09eaaa38b4
Michał Skwarek <michal.skwarek@hotmail.com>
2019-05-15 09:29:51 +0200

    #1 RED - initial commit; add basic Game test

---
+++ tests/BowlingGameTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class BowlingGameTest
+ * @package Test
+ */
+class BowlingGameTest extends TestCase
+{
+    public function testRoll()
+    {
+        $game = new Game();
+        $this->assertTrue(true);
+    }
+}
